e235dbb954e1626943719db36b0b015353879473
van-matos <vanmatos@poli.ufrj.br>
2022-07-06 23:51:08 -0300

    feat: add drop-down menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Context from "./contexts/Context";
 import { useState } from "react";
 import Home from "../src/components/Home"
+import Dropdown from "../src/components/Dropdown"
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/menu" element={<Dropdown />} />
                 </Routes>
             </BrowserRouter>
         </Context.Provider>
